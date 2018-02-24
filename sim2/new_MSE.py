b1099dfd4f06c3c6461f42bb106e3a95a10c9864
NvdYmn <navidyamini@gmail.com>
2018-02-25 00:10:55 +0100

    Add files via upload

--- sim2/new_MSE.py
+++ sim2/new_MSE.py
@@ -37,34 +37,69 @@ def MSE(target,domain,title):
     plt.title(title)
     plt.xlabel("Density")
     plt.ylabel("Q [kwh/m^2]")
+    #plt.legend(loc=3)
+    plt.show()
+    return w2,predicted_t,error
+
+def test(target,domain,w,title):
+    predicted_t = [w[0]+w[1]*x for x in domain]
+    error = []
+    for i in range(len(predicted_t)):
+        e = (math.fabs(predicted_t[i] - target[i]))**2
+        error.append(e)
+    
+    plt.plot(domain,target,marker='.')
+    plt.plot(domain,predicted_t)
+    plt.title(title)
+    plt.xlabel("Density")
+    plt.ylabel("Q [kwh/m^2]")
+    #plt.legend(loc=3)
     plt.show()
     return predicted_t,error
 
-def write_output(result,error,name):
+def write_output(result,error,name,sim_no,density_range):
     outfile = open(name+".txt", "w")
     outfile.write("density" + "," +"result" + "," + "error")
     outfile.write("\n")
     density=[]
    
-    for i in range(120):
-        density.append(1000+(i*50))
+    for i in range(sim_no):
+        density.append(1000+(i*density_range))
         outfile.write(str(density[i])+","+str(result[i])+","+str(error[i]))
         outfile.write("\n")
     outfile.close()
 
 if __name__ == "__main__":
    
+    num_train=120
+    numb_test=40
+    density_train=50
+    density_test = 174
+    
     file_name = "F:/Polito/ICTinBuildingdesign/Building_project/ouput2.txt"
     data = pd.read_csv(file_name)
-
+    
     domain = data["density"]
     target_cooling = data["cooling"]
     target_heating = data["heating"]           
-            
-    result_cooling,error1 = MSE(target_cooling ,domain,"cooling_sim2")
-    result_heting,error2 = MSE(target_heating ,domain,"heating_sim2")
+           
+    w_cooling,result_cooling,error1 = MSE(target_cooling ,domain,"cooling_sim2")
+    w_heating,result_heting,error2 = MSE(target_heating ,domain,"heating_sim2")
     
-    write_output(result_cooling,error1,"result_cooling_sim2")
-    write_output(result_heting,error2,"result_heating_sim2")
+    write_output(result_cooling,error1,"result_cooling_sim2",num_train,density_train)
+    write_output(result_heting,error2,"result_heating_sim2",num_train,density_train)
+    
+    file_test = "F:/Polito/ICTinBuildingdesign/Building_project/ouput2_test.txt"
+    testData = pd.read_csv(file_test)
+    
+    domain_test = testData["density"]
+    target_cooling_test = testData["cooling"]
+    target_heating_test = testData["heating"]
+    
+    result_cooling_test,error1_test = test(target_cooling_test ,domain_test,w_cooling,"cooling_test 2")
+    result_heting_test,error2_test = test(target_heating_test ,domain_test,w_heating,"heating_test 2")
+    
+    write_output(result_cooling_test,error1_test,"result_cooling_test 2",numb_test,density_test)
+    write_output(result_heting_test,error2_test,"result_heating_test 2",numb_test,density_test)
    
 
